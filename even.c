17cba54a114309f09b2bc60501ceaa60353e728d
jemi4 <30549886+jemi4@users.noreply.github.com>
2018-01-10 16:36:09 +0530

    even.c

---
+++ even.c
@@ -0,0 +1,18 @@
+#include<studio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("enter the number=");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("the given number is positive");
+}
+else
+{
+printf("the given number is negative");
+}
+getch();
+}
